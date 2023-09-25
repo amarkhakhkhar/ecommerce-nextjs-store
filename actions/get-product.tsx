dad2314322b364c7e56f1eb6b3faff7ef33e6b96
Amar Khakhkhar <amarkhakhkhar2241566@gmail.com>
2023-09-25 18:15:09 +0530

    complete

---
+++ actions/get-product.tsx
@@ -0,0 +1,12 @@
+import { Billboard, Product } from "@/types";
+
+const URL=`${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+const getProduct = async (id : String): Promise<Product> => {
+  const res = await fetch(`${URL}/${id}`);
+
+  return res.json()
+
+};
+
+export default getProduct;
---
+++ components/ui/button.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLElement> {
+
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type="button",
+    ...props
+}, ref)=>{
+    return (
+        <button ref={ref}
+        disabled={disabled}
+        className={cn(
+            `
+            w-auto
+            rounded-full
+            bg-black
+            border-transperent
+            px-5 
+            py-3
+            disabled:cursor-not-allowed
+            disabled:opacity-50
+            text-white
+            font-semibold
+            hover:opacity-75
+            transition
+            `,
+            className
+        )}
+        {...props}
+        >
+            {children}
+        </button>
+    )
+})
+
+Button.displayName= "Button"
+
+export default Button;
